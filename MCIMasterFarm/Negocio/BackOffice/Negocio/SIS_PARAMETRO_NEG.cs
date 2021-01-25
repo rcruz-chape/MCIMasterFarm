dc2ba15112efead552011b44707ca70406c7a8db
Renato Cruz <renato.cruz.souza@hotmail.com>
2021-01-24 23:51:12 -0300

    tr

--- MCIMasterFarm/Negocio/BackOffice/Negocio/SIS_PARAMETRO_NEG.cs
+++ MCIMasterFarm/Negocio/BackOffice/Negocio/SIS_PARAMETRO_NEG.cs
@@ -12,6 +12,8 @@ namespace MCIMasterFarm.Negocio.BackOffice.Negocio
 {
     public class SIS_PARAMETRO_NEG
     {
+        string psDS_Caracter_Obrigatorio = "S";
+        List<String> lsCaracterEspecial = new List<String>() { "@","!","$","%","¨","&","Ç","ç","?"};
         public SisParametro ObtemParametro(ref Banco pBanco)
         {
             var vSisParametroDal = new SIS_PARAMETRO_DAL();
@@ -21,6 +23,70 @@ namespace MCIMasterFarm.Negocio.BackOffice.Negocio
         {
             var vSisParametroDal = new SIS_PARAMETRO_DAL();
             return vSisParametroDal.bUpdateParametro(ref pBanco, pParametro);
+        }
+        public Boolean bValidaDSCaracter(string pdsPWd, SisParametro pParametro)
+        {
+            Boolean bValidaCaracter = pParametro.ds_car_especial != psDS_Caracter_Obrigatorio;
+            if (!bValidaCaracter)
+            {
+                foreach (string CaracterEspecial in lsCaracterEspecial)
+                {
+                    if (pdsPWd.Contains(CaracterEspecial))
+                    {
+                        bValidaCaracter = true;
+                        break;
+                    }
+                }
+            }
+            return bValidaCaracter;
+        }
+        public Boolean bValidaNumeroCaracterMaiuscula(string pdsPWd, SisParametro pParametro)
+        {
+            Boolean vbValidaNumeroCaracterMaiuscula = pParametro.ind_car_maisculo < 1;
+            if (!vbValidaNumeroCaracterMaiuscula)
+            {
+                var ContaCar = pdsPWd.Count(char.IsUpper);
+                vbValidaNumeroCaracterMaiuscula = ContaCar >= pParametro.ind_car_maisculo;
+            }
+            return vbValidaNumeroCaracterMaiuscula;
+        }
+        public Boolean bValidaNumeroCaracterMinuscula(string pdsPWd, SisParametro pParametro)
+        {
+            Boolean vbValidaNumeroCaracterMinuscula = pParametro.ind_car_minisculo < 1;
+            if (!vbValidaNumeroCaracterMinuscula)
+            {
+                var ContaCar = pdsPWd.Count(char.IsLower);
+                vbValidaNumeroCaracterMinuscula = ContaCar >= pParametro.ind_car_minisculo;
+            }
+            return vbValidaNumeroCaracterMinuscula;
+        }
+        public Boolean bValidaNumero(string pdsPWd, SisParametro pParametro)
+        {
+            int totCar = 0;
+            Boolean vbValidaNumero = pParametro.ind_numero < 1;
+            if (!vbValidaNumero)
+            {
+                totCar += pdsPWd.Count(char.IsNumber);
+                if (totCar >= pParametro.ind_numero)
+                {
+                    vbValidaNumero = true;
+                }
+            }
+            return vbValidaNumero;
+        }
+        public Boolean bValidaIndTotalCar(string pdsPWd, SisParametro pParametro)
+        {
+            int totCar = 0;
+            Boolean vbValidaIndTotalCar = pParametro.ind_total_car < 1;
+            if (!vbValidaIndTotalCar)
+            {
+                totCar += pdsPWd.Length;
+                if (totCar >= pParametro.ind_total_car)
+                {
+                    vbValidaIndTotalCar = true;
+                }
+            }
+            return vbValidaIndTotalCar;
         }
     }
 }
