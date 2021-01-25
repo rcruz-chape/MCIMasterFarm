8c6005adf3416e2629f4081cdce8e81d87c47b96
Renato Cruz <renato.cruz.souza@hotmail.com>
2021-01-24 22:56:20 -0300

    adicionada a tabela sis_parametro para a camada de negócio

---
+++ MCIMasterFarm/Negocio/BackOffice/Negocio/SIS_PARAMETRO_NEG.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCIMasterFarm.Negocio.BackOffice.DAL;
+using MCIMasterFarm.Negocio.BackOffice.Model;
+using MCIMasterFarm.Negocio.Telas;
+using MCIMasterFarm.Negocio.Global;
+
+namespace MCIMasterFarm.Negocio.BackOffice.Negocio
+{
+    public class SIS_PARAMETRO_NEG
+    {
+        public SisParametro ObtemParametro(ref Banco pBanco)
+        {
+            var vSisParametroDal = new SIS_PARAMETRO_DAL();
+            return vSisParametroDal.getParametro(ref pBanco); 
+        }
+        public Boolean bAtualizaParametro(ref Banco pBanco, SisParametro pParametro)
+        {
+            var vSisParametroDal = new SIS_PARAMETRO_DAL();
+            return vSisParametroDal.bUpdateParametro(ref pBanco, pParametro);
+        }
+    }
+}
